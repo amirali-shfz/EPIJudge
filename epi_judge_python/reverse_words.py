1875c972e99f82f573f3dfc9d5810bbb56094576
Amirali <aasharifzad2@gmail.com>
2021-01-23 21:04:48 -0500

    reverse_word mine

--- epi_judge_python/reverse_words.py
+++ epi_judge_python/reverse_words.py
@@ -8,7 +8,20 @@ from test_framework.test_utils import enable_executor_hook
 # ['r', 'a', 'm', ' ', 'i', 's', ' ', 'c', 'o', 's', 't', 'l', 'y'].
 def reverse_words(s):
     # TODO - you fill in here.
-    return
+    for i in range(len(s) // 2):
+        s[i], s[~i] = s[~i], s[i]
+    i = 0
+    while i < len(s):
+        l, r = i, i
+        while r < len(s) and s[r] != ' ':
+            r += 1
+        i = r + 1
+        r = (r - 1) if (r >= len(s) or s[r] == ' ') else r
+        while l < r:
+            s[l], s[r] = s[r], s[l]
+            r -= 1
+            l += 1
+    return s
 
 
 @enable_executor_hook
