bada75ce327f3ef036821b6429a5a5231a48684d
Amirali <aasharifzad2@gmail.com>
2021-01-23 21:42:02 -0500

    is_string_palindromic_punctuation mine

--- epi_judge_python/is_string_palindromic_punctuation.py
+++ epi_judge_python/is_string_palindromic_punctuation.py
@@ -2,7 +2,21 @@ from test_framework import generic_test
 
 
 def is_palindrome(s: str) -> bool:
-    # TODO - you fill in here.
+    l, r = 0, len(s) - 1
+
+    def isalpha(c):
+        return (ord('a') <= ord(c.lower()) <= ord('z')) or (ord('0') <= ord(c) <= ord('9'))
+
+    while l < r:
+        if not isalpha(s[l]):
+            l += 1
+        elif not isalpha(s[r]):
+            r -= 1
+        elif s[l].lower() != s[r].lower():
+            return False
+        else:
+            l, r = l+1, r-1
+
     return True
 
 
