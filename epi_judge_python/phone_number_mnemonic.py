3284af37c282421a69411d988919319c4164e7d6
Amirali <aasharifzad2@gmail.com>
2021-01-24 22:48:02 -0500

    phone_number_mnemonic mine

--- epi_judge_python/phone_number_mnemonic.py
+++ epi_judge_python/phone_number_mnemonic.py
@@ -2,10 +2,20 @@ from typing import List
 
 from test_framework import generic_test, test_utils
 
+MAPPING = ('0', '1', 'ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ')
 
 def phone_mnemonic(phone_number: str) -> List[str]:
-    # TODO - you fill in here.
-    return []
+    nums = []
+
+    def attach(cur, rem):
+        if rem == '':
+            nums.append(cur)
+            return
+        for c in MAPPING[int(rem[0])]:
+            attach(cur + c, rem[1:])
+
+    attach('', phone_number)
+    return nums
 
 
 if __name__ == '__main__':
