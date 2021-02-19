4769f432c6b9069d5f5399e50409afd4e78474c9
Amirali <aasharifzad2@gmail.com>
2021-02-19 18:09:09 -0500

    evaluate_rpn mine

--- epi_judge_python/evaluate_rpn.py
+++ epi_judge_python/evaluate_rpn.py
@@ -2,7 +2,18 @@ from test_framework import generic_test
 
 
 def evaluate(expression: str) -> int:
-    return 0
+    expr_array = expression.split(',')
+    ops = {'+': (lambda x, y: x + y), '*': (lambda x, y: x * y),
+           '/': (lambda y, x: int(x / y)), '-': (lambda y, x: x - y)}
+    stack = []
+
+    for e in expr_array:
+        if e in ops:
+            stack.append(ops[e](stack.pop(), stack.pop()))
+        else:
+            stack.append(int(e))
+
+    return stack.pop()
 
 
 if __name__ == '__main__':
