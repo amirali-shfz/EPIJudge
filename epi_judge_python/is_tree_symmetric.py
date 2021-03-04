e0219f9327a1cb507209f1185aa7488cf94ff126
Amirali Sharifzad <aasharifzad2@gmail.com>
2021-03-04 00:13:25 -0500

    is_tree_symmetric mine

--- epi_judge_python/is_tree_symmetric.py
+++ epi_judge_python/is_tree_symmetric.py
@@ -3,8 +3,20 @@ from test_framework import generic_test
 
 
 def is_symmetric(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
-    return True
+    if not tree:
+        return True
+    
+    def sym(left: BinaryTreeNode, right: BinaryTreeNode):
+        if not left and not right:
+            return True
+        if not (left and right and left.data == right.data):
+            return False
+
+        left_sym = sym(left.left, right.right)
+        right_sym = sym(left.right, right.left)
+        return left_sym and right_sym
+
+    return sym(tree.left, tree.right)
 
 
 if __name__ == '__main__':
