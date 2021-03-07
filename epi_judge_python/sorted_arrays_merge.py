ab40de0659286b25e7416df4b1ae95e69870ed6f
Amirali Sharifzad <aasharifzad2@gmail.com>
2021-03-06 23:47:35 -0500

    sorted_arrays_merge epi improved

--- epi_judge_python/sorted_arrays_merge.py
+++ epi_judge_python/sorted_arrays_merge.py
@@ -1,11 +1,11 @@
-from typing import List
+from typing import List, Tuple
 
 from test_framework import generic_test
 import heapq
 
 def merge_sorted_arrays(sorted_arrays: List[List[int]]) -> List[int]:
-    result = []
-    min_heap = []
+    min_heap: List[Tuple[int, int]] = []
+
     sorted_arrays_iters = [iter(x) for x in sorted_arrays]
 
     for i, it in enumerate(sorted_arrays_iters):
@@ -13,6 +13,7 @@ def merge_sorted_arrays(sorted_arrays: List[List[int]]) -> List[int]:
         if first is not None:
             heapq.heappush(min_heap, (first, i))
 
+    result = []
     while min_heap:
         smallest_value, smallest_index = heapq.heappop(min_heap)
         smallest_iter = sorted_arrays_iters[smallest_index]
