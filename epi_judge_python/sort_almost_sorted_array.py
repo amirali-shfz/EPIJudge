3e3012da7d6fb93804b9c9ae80ccc4267dcb09a8
Amirali Sharifzad <aasharifzad2@gmail.com>
2021-03-21 22:55:15 -0400

    sort_almost_sorted_array mine

--- epi_judge_python/sort_almost_sorted_array.py
+++ epi_judge_python/sort_almost_sorted_array.py
@@ -1,12 +1,26 @@
 from typing import Iterator, List
 
 from test_framework import generic_test
+import heapq
 
 
 def sort_approximately_sorted_array(sequence: Iterator[int],
                                     k: int) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    min_heap, result = [], []
+
+    val = next(sequence, None)
+    while k and val is not None:
+        heapq.heappush(min_heap, val)
+        val = next(sequence, None)
+        k -= 1
+
+    while min_heap:
+        result.append(heapq.heappop(min_heap))
+        if val is not None:
+            heapq.heappush(min_heap, val)
+        val = next(sequence, None)
+
+    return result
 
 
 def sort_approximately_sorted_array_wrapper(sequence, k):
