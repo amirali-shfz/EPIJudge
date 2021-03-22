2dfaa57818b415c949a5cd1518723f3962584cad
Amirali Sharifzad <aasharifzad2@gmail.com>
2021-03-21 22:34:41 -0400

    sorted_arrays_merge mine :)

--- epi_judge_python/sorted_arrays_merge.py
+++ epi_judge_python/sorted_arrays_merge.py
@@ -3,24 +3,23 @@ from typing import List, Tuple
 from test_framework import generic_test
 import heapq
 
+
 def merge_sorted_arrays(sorted_arrays: List[List[int]]) -> List[int]:
     min_heap: List[Tuple[int, int]] = []
 
-    sorted_arrays_iters = [iter(x) for x in sorted_arrays]
-
-    for i, it in enumerate(sorted_arrays_iters):
-        first = next(it, None)
-        if first is not None:
-            heapq.heappush(min_heap, (first, i))
+    sorted_arrays_iterators = [iter(x) for x in sorted_arrays]
+    for index, it in enumerate(sorted_arrays_iterators):
+        val = next(it)
+        if val is not None:
+            heapq.heappush(min_heap, (val, index))
 
     result = []
     while min_heap:
-        smallest_value, smallest_index = heapq.heappop(min_heap)
-        smallest_iter = sorted_arrays_iters[smallest_index]
-        result.append(smallest_value)
-        next_element = next(smallest_iter, None)
-        if next_element is not None:
-            heapq.heappush(min_heap, (next_element, smallest_index))
+        val, index = heapq.heappop(min_heap)
+        result.append(val)
+        next_it = next(sorted_arrays_iterators[index], None)
+        if next_it is not None:
+            heapq.heappush(min_heap, (next_it, index))
 
     return result
 
