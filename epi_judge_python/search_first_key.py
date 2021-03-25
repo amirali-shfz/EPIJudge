1d5dc4c78e4bcadf8ef740ccc37159a2dd6c1873
Amirali Sharifzad <aasharifzad2@gmail.com>
2021-03-25 18:57:36 -0400

    search_first_key mine

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -1,12 +1,21 @@
 from typing import List
-
+import bisect
 from test_framework import generic_test
 
 
 def search_first_of_k(A: List[int], k: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    L, R = 0, len(A) - 1
 
+    while L < R:
+        mid = L + (R - L) // 2
+        if A[mid] < k:
+            L = mid + 1
+        elif A[mid] == k:
+            R = mid
+        else:
+            R = mid - 1
+
+    return L if L < len(A) and A[L] == k else -1
 
 if __name__ == '__main__':
     exit(
