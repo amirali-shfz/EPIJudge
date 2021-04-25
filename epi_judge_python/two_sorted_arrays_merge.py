d3d70aabfabd50d9bf103ba9f9e61335fe19593c
Amirali <aasharifzad2@gmail.com>
2021-04-25 15:03:59 -0400

    two_sorted_arrays_merge whatever

--- epi_judge_python/two_sorted_arrays_merge.py
+++ epi_judge_python/two_sorted_arrays_merge.py
@@ -5,7 +5,18 @@ from test_framework import generic_test
 
 def merge_two_sorted_arrays(A: List[int], m: int, B: List[int],
                             n: int) -> None:
-    # TODO - you fill in here.
+    a, b, write_index = m-1, n-1, m + n - 1
+
+    while b >= 0:
+        if a >= 0 and A[a] > B[b]:
+            A[write_index] = A[a]
+            a -= 1
+        else:
+            A[write_index] = B[b]
+            b -= 1
+
+        write_index -= 1
+
     return
 
 
