b35454b1240e0259b6d1cf5a332f6afddbabee4d
Amirali <aasharifzad2@gmail.com>
2021-04-28 14:18:04 -0400

    check_bst recursion

--- epi_judge_python/is_tree_a_bst.py
+++ epi_judge_python/is_tree_a_bst.py
@@ -3,8 +3,14 @@ from test_framework import generic_test
 
 
 def is_binary_tree_bst(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
-    return True
+    def matches_constraints(node, lower=float('-inf'), higher=float('inf')):
+        if not node:
+            return True
+        if not lower <= node.data <= higher:
+            return False
+        return matches_constraints(node.left, lower, node.data) and matches_constraints(node.right, node.data, higher)
+
+    return matches_constraints(tree)
 
 
 if __name__ == '__main__':
--- epi_judge_python/test_framework/binary_tree_utils.py
+++ epi_judge_python/test_framework/binary_tree_utils.py
@@ -149,6 +149,56 @@ def assert_equal_binary_trees(expected, result):
             s.append((expected.right, result.right, path.with_right()))
 
 
+def binary_tree_to_string_pretty(root, val="data", left="left", right="right"):
+    def display(root, val=val, left=left, right=right):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if getattr(root, right) is None and getattr(root, left) is None:
+            line = '%s' % getattr(root, val)
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if getattr(root, right) is None:
+            lines, n, p, x = display(getattr(root, left))
+            s = '%s' % getattr(root, val)
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if getattr(root, left) is None:
+            lines, n, p, x = display(getattr(root, right))
+            s = '%s' % getattr(root, val)
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = display(getattr(root, left))
+        right, m, q, y = display(getattr(root, right))
+        s = '%s' % getattr(root, val)
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+    lines, *_ = display(root, val, left, right)
+    return '\n'.join(lines)
+
+
 def binary_tree_to_string(tree):
     result = ''
     nodes = collections.deque()
