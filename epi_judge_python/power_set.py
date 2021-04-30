6fcd213b379b2d85c4741dc80ecf1f4864176c4a
Amirali <aasharifzad2@gmail.com>
2021-04-30 12:52:05 -0400

    power_set mine

--- epi_judge_python/power_set.py
+++ epi_judge_python/power_set.py
@@ -3,14 +3,11 @@ from typing import List
 from test_framework import generic_test, test_utils
 
 
-# 
 def generate_power_set(input_set: List[int]) -> List[List[int]]:
     def add_remainder(cur, rem):
         sol.append(cur)
         for i, num in enumerate(rem):
-            new_cur = list(cur)
-            new_cur.append(num)
-            add_remainder(new_cur, rem[i+1:])
+            add_remainder(cur+[num], rem[i+1:])
 
     sol = []
     add_remainder([], input_set)
