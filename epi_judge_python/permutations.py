90998a54b3d8df7c0c141f4043b8eda9c03595ae
Amirali <aasharifzad2@gmail.com>
2021-04-30 02:04:39 -0400

    permutations mine

--- epi_judge_python/permutations.py
+++ epi_judge_python/permutations.py
@@ -4,8 +4,20 @@ from test_framework import generic_test, test_utils
 
 
 def permutations(A: List[int]) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    def permute(cur, remaining):
+        if not remaining:
+            sol.append(cur)
+            return
+        for num in remaining:
+            new_cur = list(cur)
+            new_cur.append(num)
+            new_rem = list(remaining)
+            new_rem.remove(num)
+            permute(new_cur, new_rem)
+
+    sol = []
+    permute([], A)
+    return sol
 
 
 if __name__ == '__main__':
