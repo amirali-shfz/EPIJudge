00cd82e910891155730ec5167dc09aa2335be2d5
Amirali <aasharifzad2@gmail.com>
2021-05-01 23:36:54 -0400

    number_of_score_combinations epi

--- epi_judge_python/number_of_score_combinations.py
+++ epi_judge_python/number_of_score_combinations.py
@@ -5,8 +5,19 @@ from test_framework import generic_test
 
 def num_combinations_for_final_score(final_score: int,
                                      individual_play_scores: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    num_combs = [[1] + [0] * final_score
+                 for _ in individual_play_scores]
+
+    for i, play_score in enumerate(individual_play_scores):
+        for cur_score in range(1, final_score + 1):
+            without = num_combs[i - 1][cur_score] if play_score >= 0 else 0
+            withit = (num_combs[i][cur_score - play_score]
+                      if cur_score >= play_score else 0)
+
+            num_combs[i][cur_score] = without + withit
+
+    return num_combs[-1][-1]
+
 
 
 if __name__ == '__main__':
