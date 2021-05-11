fb620a37125fe81c7b967070519045a524aef2bf
Amirali <aasharifzad2@gmail.com>
2021-05-10 23:55:24 -0400

    matrix_connected_regions mine

--- epi_judge_python/matrix_connected_regions.py
+++ epi_judge_python/matrix_connected_regions.py
@@ -6,7 +6,7 @@ import collections
 Cord = collections.namedtuple('Cord', ('x', 'y'))
 
 
-def flip_color(X: int, Y: int, image: List[List[bool]]) -> None:
+def flip_color_dfs(X: int, Y: int, image: List[List[bool]]) -> None:
     val = image[X][Y]
 
     def flip(c):
@@ -16,6 +16,20 @@ def flip_color(X: int, Y: int, image: List[List[bool]]) -> None:
         [flip(Cord(c.x+d[0], c.y+d[1])) for d in [(-1, 0), (1, 0), (0, -1), (0, 1)]]
 
     flip(Cord(X, Y))
+    return
+
+
+def flip_color(X: int, Y: int, image: List[List[bool]]) -> None:
+    val, q = image[X][Y], collections.deque()
+    q.append(Cord(X, Y))
+    while q:
+        c = q.popleft()
+        image[c.x][c.y] = not val
+        for d in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            new_x, new_y = c.x+d[0], c.y+d[1]
+            if 0 <= new_x < len(image) and 0 <= new_y < len(image[0]) and image[new_x][new_y] == val:
+                q.append(Cord(new_x, new_y))
+
     return
 
 
