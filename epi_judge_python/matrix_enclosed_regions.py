5def5e7156563d57d60482d740e6c6bf5a0b87df
Amirali <aasharifzad2@gmail.com>
2021-05-11 00:22:12 -0400

    matrix_enclosed_regions mine

--- epi_judge_python/matrix_enclosed_regions.py
+++ epi_judge_python/matrix_enclosed_regions.py
@@ -1,10 +1,31 @@
 from typing import List
 
 from test_framework import generic_test
+import collections
 
 
 def fill_surrounded_regions(board: List[List[str]]) -> None:
-    # TODO - you fill in here.
+    Coord = collections.namedtuple('Coord', ('x', 'y'))
+
+    def get_white_edge_coords(x_len, y_len):
+        return [Coord(x, y) for y in range(y_len) for x in [0, x_len-1] if board[x][y] == "W"] + \
+               [Coord(x, y) for x in range(x_len) for y in [0, y_len-1] if board[x][y] == "W"]
+    edge_coords = get_white_edge_coords(len(board), len(board[0]))
+
+    q = collections.deque(edge_coords)
+
+    while q:
+        c = q.popleft()
+        board[c.x][c.y] = "R"
+        for d in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            new_x, new_y = c.x + d[0], c.y + d[1]
+            if 0 <= new_x < len(board) and 0 <= new_y < len(board[0]) and board[new_x][new_y] == "W":
+                q.append(Coord(new_x, new_y))
+
+    for x in range(len(board)):
+        for y in range(len(board[0])):
+            board[x][y] = "W" if board[x][y] == "R" else "B"
+
     return
 
 
