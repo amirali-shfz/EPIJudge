dda3b1c6ea86ae1f171a65f9b3ce72a0b5b4c127
Amirali <aasharifzad2@gmail.com>
2021-05-10 23:37:44 -0400

    matrix_connected_regions mine

--- epi_judge_python/matrix_connected_regions.py
+++ epi_judge_python/matrix_connected_regions.py
@@ -1,10 +1,21 @@
 from typing import List
 
 from test_framework import generic_test
+import collections
+
+Cord = collections.namedtuple('Cord', ('x', 'y'))
 
 
-def flip_color(x: int, y: int, image: List[List[bool]]) -> None:
-    # TODO - you fill in here.
+def flip_color(X: int, Y: int, image: List[List[bool]]) -> None:
+    val = image[X][Y]
+
+    def flip(c):
+        if not (0 <= c.x < len(image) and 0 <= c.y < len(image[0]) and image[c.x][c.y] == val):
+            return
+        image[c.x][c.y] = not image[c.x][c.y]
+        [flip(Cord(c.x+d[0], c.y+d[1])) for d in [(-1, 0), (1, 0), (0, -1), (0, 1)]]
+
+    flip(Cord(X, Y))
     return
 
 
