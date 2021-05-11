b28f2490f4b1b83204f4923c1076ab5863060d00
Amirali <aasharifzad2@gmail.com>
2021-05-11 00:38:54 -0400

    matrix_enclosed_regions epi

--- epi_judge_python/matrix_enclosed_regions.py
+++ epi_judge_python/matrix_enclosed_regions.py
@@ -6,13 +6,9 @@ import collections
 
 def fill_surrounded_regions(board: List[List[str]]) -> None:
     Coord = collections.namedtuple('Coord', ('x', 'y'))
-
-    def get_white_edge_coords(x_len, y_len):
-        return [Coord(x, y) for y in range(y_len) for x in [0, x_len-1] if board[x][y] == "W"] + \
-               [Coord(x, y) for x in range(x_len) for y in [0, y_len-1] if board[x][y] == "W"]
-    edge_coords = get_white_edge_coords(len(board), len(board[0]))
-
-    q = collections.deque(edge_coords)
+    n, m = len(board), len(board[0])
+    q = collections.deque([Coord(x, y) for y in range(m) for x in [0, n-1] if board[x][y] == "W"] +
+                          [Coord(x, y) for x in range(n) for y in [0, m-1] if board[x][y] == "W"])
 
     while q:
         c = q.popleft()
@@ -22,10 +18,7 @@ def fill_surrounded_regions(board: List[List[str]]) -> None:
             if 0 <= new_x < len(board) and 0 <= new_y < len(board[0]) and board[new_x][new_y] == "W":
                 q.append(Coord(new_x, new_y))
 
-    for x in range(len(board)):
-        for y in range(len(board[0])):
-            board[x][y] = "W" if board[x][y] == "R" else "B"
-
+    board[:] = [["W" if cell == "R" else "B" for cell in row] for row in board]
     return
 
 
